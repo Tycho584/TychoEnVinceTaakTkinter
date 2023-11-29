d45df7b4253bb39ca0494e47a91e8ffe6f487628
tycho584 <tycho.killaars@outlook.com>
2023-11-29 09:52:31 +0100

    Backend Tab1

--- TychoOnlyTkinterAssignment.py
+++ TychoOnlyTkinterAssignment.py
@@ -4,9 +4,7 @@ import requests
 
 root = tk.Tk()
 
-root.iconbitmap("Afbeeldingen\pay_cash_payment_money_dollar_bill_icon_143267.ico")
-
-###############################################################################################################################################################################################
+baseURL = "https://api.frankfurter.app/2023-01-01..2023-11-22"
 
 def specificDate():
     baseURL = "https://api.frankfurter.app/"
@@ -37,28 +35,15 @@ def excangeCurrency():
 
     comingFromCurrencyAmount = fromCurrencyAmount.get()
 
-    print(comingFromCurrencyAmount)
+    URL = baseURL+f"latest?from={comingFromCurrency}&to={goingToCurrency}&amount={comingFromCurrencyAmount}"
 
-    if comingFromCurrencyAmount.isnumeric() == False or comingFromCurrencyAmount == "":
-        noNumbersGivvenInAmountLabel.config(text="Give in a number in the entry!!", bg="yellow", fg="red")
+    getFileContent = requests.get(URL)
+    getFileContentJson = getFileContent.json()
+    print(URL)
+    print(getFileContentJson)
 
-    else:
-        print(fromCurrencyAmount.get())
-        comingFromCurrencyAmount = fromCurrencyAmount.get()
-
-        URL = baseURL+f"latest?from={comingFromCurrency}&to={goingToCurrency}&amount={comingFromCurrencyAmount}"
-
-        getFileContent = requests.get(URL)
-        getFileContentJson = getFileContent.json()
-        print(URL)
-        print(getFileContentJson)
-
-        toCurrencyAmount.insert(0,str(getFileContentJson["rates"][goingToCurrency]))
-
-        noNumbersGivvenInAmountLabel.config(text="", bg="white")
-
-
-
+    toCurrencyAmount.insert(0,str(getFileContentJson["rates"][goingToCurrency]))
+w1
 def latestInformation():
     URL = "https://api.frankfurter.app/latest"
 
@@ -75,7 +60,6 @@ def showAvailableCurrencies():
 
     print(getFileContentAvailableCurrenciesJson)
 
-###################################################################################################################################################################################################
 root.title("Currency Excanger")
 root.geometry("1000x650")
 
@@ -102,36 +86,24 @@ toValueInside = tk.StringVar(tab1)
 welcomeLabel = tk.Label(tab1, text="Welcome to the tkinter assignment of Tycho & Vince")
 fromToWelcomeLabel = tk.Label(tab1, text="In this tab you can view the excange rate for diffrent currencies.")
 
-fromFullCurrencyNameLabel = tk.Label(tab1, text="",)
-toFullCurrencyNameLabel = tk.Label(tab1, text="")
-
 fromCurrency = tk.OptionMenu(tab1,fromValueInside, *currencies)
 toCurrency = tk.OptionMenu(tab1, toValueInside, *currencies)
 
 fromCurrencyAmount = tk.Entry(tab1)
-EqualsSignLabel = tk.Label(tab1, text="=")
 toCurrencyAmount = tk.Entry(tab1)
-
-noNumbersGivvenInAmountLabel = tk.Label(tab1,text="")
 
 exchangeButton = tk.Button(tab1, text="Click here to exchange the currency", command=excangeCurrency)
 
 
-welcomeLabel.grid(row=0, column=0, columnspan=6)
-fromToWelcomeLabel.grid(row=1, column=0, columnspan=6)
+welcomeLabel.grid(row=1, column=0, columnspan=6)
+fromToWelcomeLabel.grid(row=2, column=0, columnspan=6)
 
-fromFullCurrencyNameLabel.grid(row=3, column=0, columnspan=2)
-toFullCurrencyNameLabel.grid(row=3, column=2,columnspan=2)
+fromCurrency.grid(row=3, column=0, columnspan=2)
+toCurrency.grid(row=3, column=4, columnspan=2)
 
-fromCurrency.grid(row=2, column=0, columnspan=2)
-toCurrency.grid(row=2, column=5, columnspan=2)
+fromCurrencyAmount.grid(row=3, column=2)
+toCurrencyAmount.grid(row=3, column=3)
 
-fromCurrencyAmount.grid(row=2, column=2)
-EqualsSignLabel.grid(row=2,column=3)
-toCurrencyAmount.grid(row=2, column=4)
-
-exchangeButton.grid(row=4, column=2, columnspan=3)
-
-noNumbersGivvenInAmountLabel.grid(row=5,column=0,columnspan=6)
+exchangeButton.grid(row=4, column=2, columnspan=2)
 
 root.mainloop()
